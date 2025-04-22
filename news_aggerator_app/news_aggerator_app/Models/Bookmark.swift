3f1f6bfcb60882514adbaa1d52f704fbb50399d7
Sasa <W2151411@westminster.ac.uk>
2025-04-23 01:03:58 +0800

    Updated Core data missing attribute

--- news_aggerator_app/news_aggerator_app/Models/Bookmark.swift
+++ news_aggerator_app/news_aggerator_app/Models/Bookmark.swift
@@ -4,23 +4,24 @@
 //
 //  Created by user271739 on 4/19/25.
 //
-
-import Foundation
-import CoreData
-
-@objc(Bookmark)
-public class Bookmark: NSManagedObject {
-    @NSManaged public var id: UUID?
-    @NSManaged public var title: String?
-    @NSManaged public var desc: String?
-    @NSManaged public var url: String?
-    @NSManaged public var urlToImage: String?
-    @NSManaged public var publishedAt: String?
-    @NSManaged public var content: String?
-}
-
-extension Bookmark {
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Bookmark> {
-        return NSFetchRequest<Bookmark>(entityName: "Bookmark")
-    }
-}
+/*
+ import Foundation
+ import CoreData
+ 
+ @objc(Bookmark)
+ public class Bookmark: NSManagedObject {
+ @NSManaged public var id: UUID?
+ @NSManaged public var title: String?
+ @NSManaged public var desc: String?
+ @NSManaged public var url: String?
+ @NSManaged public var urlToImage: String?
+ @NSManaged public var publishedAt: String?
+ @NSManaged public var content: String?
+ }
+ 
+ extension Bookmark {
+ @nonobjc public class func fetchRequest() -> NSFetchRequest<Bookmark> {
+ return NSFetchRequest<Bookmark>(entityName: "Bookmark")
+ }
+ }
+ */
